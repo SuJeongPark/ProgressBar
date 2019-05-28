51fe920b53f98a1189c3fc6f761f0c255f8570af
Sujeong Park <s2017s08@e-mirim.hs.kr>
2019-05-28 11:52:27 +0900

    #2 ProgressBar 및 버튼 동작 코드 구현

--- ProgressBar/Form1.cs
+++ ProgressBar/Form1.cs
@@ -9,11 +9,50 @@ using System.Windows.Forms;
 
 namespace ProgressBar
 {
+
     public partial class Form1 : Form
     {
+        int Num = 0;
+        string OrgStr = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblStatus.Text;
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.Num++;
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr + Num + "%";
+
+            if (this.Num >= 100)
+            {
+                this.Timer.Enabled = false;
+                return;
+            }
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = false;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.Num = 0;
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr;
         }
     }
 }
